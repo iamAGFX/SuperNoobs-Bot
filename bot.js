942565f4f58b19f15a85fabd2bf2b1e6ebfbb085
iamAGFX <48972047+iamAGFX@users.noreply.github.com>
2019-03-27 16:32:10 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-const { Client, RichEmbed } = require('discord.js');
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const { Client, RichEmbed } = require('discord.js');
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
