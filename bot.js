95b295b41dc5f8964f88831dad87870b9f9c4eac
iamAGFX <48972047+iamAGFX@users.noreply.github.com>
2019-03-27 16:42:50 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
 
 client.on('ready', () => {
@@ -163,4 +163,4 @@ client.on('message', (receivedMessage) => {
 });
 
 
-client.login(process.env.token)
+client.login(process.env.BOT_TOKEN)
